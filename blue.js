465e777997e040cc61682fbc6b9d0113a98524ca
james simon <swaggysimon24@gmail.com>
2025-02-05 15:23:25 -0800

    add stars to blue.js

--- blue.js
+++ blue.js
@@ -39,6 +39,26 @@ scene.add(ambLight);
 
 const textureLoader = new THREE.TextureLoader();
 
+let shootingStars = [];
+let deltaElev = 25;
+const glowTexture = textureLoader.load('./images/cometTrail.png');
+const glowMaterial = new THREE.SpriteMaterial({ map: glowTexture, color: 0xffffff, blending: THREE.AdditiveBlending });
+
+function createComet(size, elevation) {
+    const cometGeo = new THREE.SphereGeometry(size / 2, 6, 6);
+    const cometMat = new THREE.MeshBasicMaterial({ map: textureLoader.load('./images/white.png') });
+    const comet = new THREE.Mesh(cometGeo, cometMat);
+    comet.position.set(-100, elevation - 5, 0);
+    scene.add(comet);
+
+    const glowSprite = new THREE.Sprite(glowMaterial); // Reuse material
+    glowSprite.scale.set(100 * size, 2.5 * size, size);
+    glowSprite.position.copy(comet.position);
+    scene.add(glowSprite);
+
+    shootingStars.push({ star: comet, light: glowSprite });
+}
+
 const sunGeo = new THREE.SphereGeometry(16, 30, 30);
 const sunMat = new THREE.MeshBasicMaterial({
     map: textureLoader.load('./images/blue.png')  // Link
@@ -203,6 +223,23 @@ function animate() {
 
     composer.render();
     requestAnimationFrame(animate);
+
+    let n = Math.random(); 
+    
+    if (n < 0.001) { 
+        createComet(n * 1000, (-1 * deltaElev) + (n * 100000)); 
+    } 
+    
+    shootingStars = shootingStars.filter(comet => {
+        if (comet.star.position.x > 75) {
+            scene.remove(comet.star);
+            scene.remove(comet.light);
+            return false; // Remove from the array
+        }
+        comet.star.position.x += 1;
+        comet.light.position.copy(comet.star.position);
+        return true; // Keep in the array
+    });  
 }
 animate();
 
@@ -210,6 +247,14 @@ window.addEventListener('resize', function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+window.addEventListener('keydown', function (event) { 
+    if (event.code === 'Space') { 
+        const size = Math.random() * 0.5 + 0.1; 
+        const elevation = Math.random() * 40 - 20; 
+        createComet(size, elevation); 
+    }
 });
 
 const monitor = document.getElementById('monitor'); 
